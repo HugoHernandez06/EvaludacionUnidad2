40678b4d7b718ff29572667c7a3c2e9c8a4f0480
HugoHernandez06 <hugoa.hernandez@upb.edu.co>
2021-09-28 19:13:07 -0700

    Accion de Mejora
    Se elimino el pidC=wait(&status) del main.c y se agrego en el program.c

--- Problema2/program.c
+++ Problema2/program.c
@@ -2,22 +2,22 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdio.h>
+
 int main(int argc, char *argv[]){
-
-printf("Soy el proceso: %d\n",getpid());
-
-FILE * flujo=fopen("datos.txt","r");
-if(flujo==NULL){
-perror("Error apertura archivo");
-
-}
-char caracter;
-while(feof(flujo)==0){
-caracter=fgetc(flujo);
-printf("%c", caracter);
-}
-fclose(flujo);
-printf("se leyo correctamente el archivo.....\n");
-
+    pid_t pidC;
+    int status;
+    printf("Soy el proceso: %d\n",getpid());
+    pidC = wait(&status);
+    FILE * flujo=fopen("datos.txt","r");
+    if(flujo==NULL){
+        perror("Error apertura archivo");
+    }
+    char caracter;
+    while(feof(flujo)==0){
+        caracter=fgetc(flujo);
+        printf("%c", caracter);
+    }
+    fclose(flujo);
+    printf("se leyo correctamente el archivo.....\n");
 }
 
